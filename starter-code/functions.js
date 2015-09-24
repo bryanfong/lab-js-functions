a55a6d947baff3ebb1d39323ab0444cb60f6cdb8
Bryan Fong <bryan.fch@gmail.com>
2015-09-24 23:45:47 +0800

    done

--- starter-code/functions.js
+++ starter-code/functions.js
@@ -1,53 +1,83 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(a,b) {
+    if(a>b){
+        return a
+    }else{
+        return b
+    }
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(a,b,c) {
+    if(a>b>c){
+        return a
+    }else if(b>a>c){
+        return b
+    }else{
+        return c        
+    }
 }
+
 
 // Question 3
-function isCharacterAVowel() {
-
-}
-
-// Question 4
-function sumArray() {
- 
-}
-
+function isCharacterAVowel(letter) {
+    if(letter=="a"||letter=="e"||letter=="i"||letter=="o"||letter=="u"){
+        return true
+    }else{
+        return false        
+    }
+};
 
 // Question 4
-function multiplyArray() {
+function sumArray(sth) {
+     var sum = 0;    
+     for(var i=0; i<sth.length; i++){
+         sum += sth[i];
+     }   
+    console.log(sum);
+}
 
+
+// Question 4
+function multiplyArray(sth) {
+     var result = 1;    
+     for(var i=0; i<sth.length; i++){
+         result *= sth[i];
+     }   
+    console.log(result);
 }
 
 
 // Question 5
-var numberOfArguments = function(){
-  
+
+function numberOfArguments() {
+    return arguments.length;
+}
+
+// Question 6
+function reverseString(str){
+ return str.split("").reverse().join(" ");
 }
 
 
 
-// Question 6
-var reverseString = function (){
-  
-};
-
-
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord (sth) {
+    sth.sort();
+    console.log(sth[sth.length-1]) 
 }
 
 
 // Question 8
-function filterLongWords () {
-  
-}
+function filterLongWords (sth, num) {
+    sth.sort();
+
+    for(var whatever in sth){
+	    if(sth[whatever].length > num){
+	        console.log(sth[whatever]);
+	    }    
+    }
+};
 
 
 // Bonus 1
